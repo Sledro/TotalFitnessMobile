b88e7c3b027695c0ec6856144cb2b09a4a3a6d3a
sledro <dan@sledro.com>
2018-03-19 18:59:14 +0000

    Loading screen added.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+export class HomePage{
+  constructor(public navCtrl: NavController,private iab: InAppBrowser /* 2 */) {
+    this.iab.create('http://totalfitness.host','_self',{location:'no'}); /*3*/
   }
-
 }
